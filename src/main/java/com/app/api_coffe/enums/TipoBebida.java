22d4b8f0231abe91a3341d0b70c1b647d5138716
WerbethMartins <martinspaulinho91@gmail.com>
2026-04-11 15:46:26 -0300

    Criado model CafeRegistro e Usuario, criado enum TipoBebida. Adicionado métodos auxiliares na entidade Usuario

---
+++ src/main/java/com/app/api_coffe/enums/TipoBebida.java
@@ -0,0 +1,13 @@
+package com.app.api_coffe.enums;
+
+public enum TipoBebida {
+    ESPRESSO,
+    CAPUCCINO,
+    LATTE,
+    COLD_BREW,
+    POUR_OVER,
+    AEROPRESS,
+    FRENCH_PRESS,
+    CHEMEX,
+    OUTRO
+}
